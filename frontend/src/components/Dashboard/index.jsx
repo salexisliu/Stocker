3d48dcee7046f946d48449441e2bd55b1b71a3fb
Damon Chu <dc3800@nyu.edu>
2025-11-05 19:15:38 -0500

    added color palette

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from './Header';
+import Inventory from './Inventory';
+import Search from './Search';
+import ShoppingList from './ShoppingList';
+import Expiring from './Expiring';
 
 const Dashboard = () => {
-  return <div className='dashboard'>Dashboard</div>;
+  return <div className='dashboard'>
+    <Header />
+    <Inventory />
+    <Search />
+    <ShoppingList />
+    <Expiring />
+  </div>;
 };
 
 export default Dashboard;
