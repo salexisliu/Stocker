674fbaed839aff9e67cee3b0ba668b6990080217
Damon Chu <dc3800@nyu.edu>
2025-11-08 12:01:53 -0500

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,10 +19,10 @@ function App() {
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx </code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p className='read-the-docs'>Cli on the Vite and React logos to learn more</p>
     </>
   );
 }
